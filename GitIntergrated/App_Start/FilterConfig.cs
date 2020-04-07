8c02fe1054ec3189a32d3eedf478a84f25c87d17
hoangnv230395 <hoangnv230395@gmail.com>
2020-04-07 11:10:37 +0700

    Add project files.

---
+++ GitIntergrated/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace GitIntergrated
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
